8e9551d3e5e5b8267733040c21107bc762b51448
armalite <ttc_rulz@hotmail.com>
2023-05-05 18:07:10 +1200

    Project files

---
+++ Makefile-common.mk
@@ -0,0 +1,69 @@
+MAKEFLAGS += --warn-undefined-variables
+SHELL = /bin/bash -o pipefail
+.DEFAULT_GOAL := help
+.PHONY: help clean install format check lint pyright test dist hooks install-hooks
+
+## display help message
+help:
+	@awk '/^##.*$$/,/^[~\/\.0-9a-zA-Z_-]+:/' $(MAKEFILE_LIST) | awk '!(NR%2){print $$0p}{p=$$0}' | awk 'BEGIN {FS = ":.*?##"}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}' | sort
+
+venv ?= .venv
+pip := $(venv)/bin/pip
+
+$(pip):
+# create venv using system python even when another venv is active
+	PATH=$${PATH#$${VIRTUAL_ENV}/bin:} python3 -m venv --clear $(venv)
+	$(venv)/bin/python --version
+	$(pip) install pip~=23.0 wheel~=0.40
+
+$(venv): pyproject.toml $(pip)
+	$(pip) install -e '.[dev]'
+	touch $(venv)
+
+# delete the venv
+clean:
+	rm -rf $(venv)
+
+## create venv and install this package and hooks
+install: $(venv) node_modules $(if $(value CI),,install-hooks)
+
+## lint code and run static type check
+check: lint pyright
+
+## lint and format code
+lint: $(venv)
+	SKIP=pyright,test $(venv)/bin/pre-commit run --show-diff-on-failure --color=always --all-files --hook-stage push
+
+node_modules: package.json
+	npm install --no-save
+	touch node_modules
+
+## pyright
+pyright: node_modules $(venv)
+# activate venv so pyright can find dependencies
+	PATH="$(venv)/bin:$$PATH" node_modules/.bin/pyright
+
+## run tests
+test: $(venv)
+	$(venv)/bin/pytest
+
+## build python distribution
+dist: $(venv)
+	rm -rf build dist *.egg-info
+	$(venv)/bin/python -m build --sdist --wheel
+
+## publish to pypi
+publish: $(venv)
+	$(venv)/bin/twine upload dist/*
+
+## run pre-commit git hooks on all files
+hooks: $(venv)
+	$(venv)/bin/pre-commit run --show-diff-on-failure --color=always --all-files --hook-stage push
+
+install-hooks: .git/hooks/pre-commit .git/hooks/pre-push
+
+.git/hooks/pre-commit: $(venv)
+	$(venv)/bin/pre-commit install -t pre-commit
+
+.git/hooks/pre-push: $(venv)
+	$(venv)/bin/pre-commit install -t pre-push
---
+++ README.md
@@ -0,0 +1,7 @@
+# dbt-ai
+
+GPT powered DBT helper bot
+
+## Contributing
+
+See [CONTRIBUTING.md](CONTRIBUTING.md) to get started and develop in this repo.
