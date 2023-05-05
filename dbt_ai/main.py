bae8b5450732dd60eedcb0cb899905e1a2927aac
armalite <ttc_rulz@hotmail.com>
2023-05-05 18:11:04 +1200

    Example call to openai

--- dbt_ai/main.py
+++ dbt_ai/main.py
@@ -2,6 +2,26 @@ import sys
 from typing import List
 
 
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="GPT-3.5",  
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    return response.choices[0].text.strip()
+
+def suggest_dbt_model_improvements(file_path):
+    with open(file_path, "r") as f:
+        content = f.read()
+
+    prompt = f"Given the following dbt model:\n\n{content}\n\nPlease provide suggestions on how to improve this model:"
+    response = generate_response(prompt)
+    return response
+
+
 def main(args: List[str] = sys.argv[1:]) -> None:
     print(f"hello {args[0]}!")
 
