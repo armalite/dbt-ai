4306f3a589ec576ed357e953bfcdaacfdfe1e040
armalite <ttc_rulz@hotmail.com>
2023-05-06 17:32:35 +1200

    Update venv install to use Python3.10

--- Makefile-common.mk
+++ Makefile-common.mk
@@ -12,8 +12,8 @@ pip := $(venv)/bin/pip
 
 $(pip):
 # create venv using system python even when another venv is active
-	PATH=$${PATH#$${VIRTUAL_ENV}/bin:} python3 -m venv --clear $(venv)
-	$(venv)/bin/python --version
+	PATH=$${PATH#$${VIRTUAL_ENV}/bin:} python3.10 -m venv --clear $(venv)
+	$(venv)/bin/python3.10 --version
 	$(pip) install pip~=23.0 wheel~=0.40
 
 $(venv): pyproject.toml $(pip)
